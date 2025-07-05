fab27a3bf5b268098775cb6827eae73ce5003510
Tirthmandaliya <142874808+Tirthmandaliya@users.noreply.github.com>
2025-07-05 00:18:52 -0700

    new chages add

--- chatFrontend/src/lib/axios.js
+++ chatFrontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL:"https://real-time-chat-application-2-53hv.onrender.com/api",
+    baseURL:"https://real-time-chat-application-pr87.onrender.com",
     withCredentials:true,
 });
 
